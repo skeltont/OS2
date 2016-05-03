09d1ef01ada08557d382cd2875a6bf647f4ce4f6
Ty Skelton <skeltont@oregonstate.edu>
2016-05-03 14:42:30 -0700

    adds semi working concurrency

---
+++ projects/conc3/conc.c
@@ -0,0 +1,150 @@
+/*
+ *  	Author: Ty Skelton
+ *  	Class: CS444 - Operating Systems 2
+ *  	Assignment: Concurrency 3
+ *  	Solution:
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define KNRM  	"\x1B[0m"
+#define KRED  	"\x1B[31m"
+#define KGRN  	"\x1B[32m"
+#define KYEL  	"\x1B[33m"
+#define RESET 	"\033[0m"
+
+#define LIST_SIZE 	5
+#define SEARCHERS	3
+#define INSERTERS	1
+#define DELETERS	1
+
+struct linkedList {
+	int num;
+	sem_t lock;
+	struct linkedList *next;
+};
+
+void initListItem (struct linkedList *lst, int count)
+{
+	lst->next = malloc(sizeof(struct linkedList));
+	sem_init(&lst->lock, 0, 1);
+	lst->num = count;
+}
+
+struct linkedList *createLinkedListItem(int count)
+{
+	struct linkedList *newList = malloc(sizeof(struct linkedList));
+	initListItem(newList, count);
+	return(newList);
+}
+
+void generateList(struct linkedList *init)
+{
+	struct linkedList *curr, *prev;
+	for (int i = 0; i < LIST_SIZE; i++) {
+		curr = createLinkedListItem(i + 1);
+		switch (i) {
+			case 0:
+				init->next = curr;
+				break;
+			case LIST_SIZE - 1:
+				curr->next = init;
+				prev->next = curr;
+				break;
+			default:
+				prev->next = curr;
+				break;
+		}
+		prev = curr;
+	}
+}
+
+void *searcher(void *head) {
+	struct linkedList *curr;
+	int lock_status;
+	curr = head;
+	for (;;) {
+		sem_getvalue(&curr->lock, &lock_status);
+		if (lock_status == 0) {
+			printf(KYEL "SEARCHER: read %d\n" RESET, curr->num);
+			curr = curr->next;
+		} else {
+			printf(KYEL "SEARCHER: found locked link, skipping over series.\n");
+			curr = curr->next->next->next;
+		}
+		sleep(1);
+	}
+}
+
+void *inserter(void *head) {
+	struct linkedList *curr, *prev;
+	int lock_status;
+
+	prev = head;
+	curr = prev->next;
+	printf("prev %d, curr %d\n", prev->num, curr->num);
+
+	for (;;) {
+		sem_getvalue(&curr->lock, &lock_status);
+		if (lock_status == 0) {
+			if ((rand() % 7 ) == 0) {
+				sem_wait(&prev->lock);
+				sem_wait(&curr->lock);
+				sem_wait(&curr->next->lock);
+
+				printf(KGRN "INSERTER: inserting, locking %d, %d, %d.\n" RESET,
+					prev->num, curr->num, curr->next->num);
+				sleep (10);
+				printf(KGRN "INSERTER: releasing.\n" RESET);
+
+				sem_post(&prev->lock);
+				sem_post(&curr->lock);
+				sem_post(&curr->next->lock);
+				curr = curr->next;
+			}
+		} else {
+			printf(KGRN "INSERTER: found locked link, skipping over series.\n" RESET);
+			curr = curr->next->next->next;
+		}
+		prev = curr;
+	}
+}
+
+// abstract out code to wait for threads to terminate
+void cleanUpThreads(pthread_t *procs, int count) {
+	for (int i = 0; i < count; i++) {
+		pthread_join(procs[i], NULL);
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	struct linkedList *init;
+	int i;
+	pthread_t search_procs[SEARCHERS],
+		  insert_procs[INSERTERS],
+		  delete_procs[DELETERS];
+
+	// initialize our singly-linked list stuff
+	init = createLinkedListItem(0);
+	generateList(init);
+
+	// spin up searchers
+	for (i = 0; i < SEARCHERS; i++) {
+		pthread_create(&search_procs[i], NULL, searcher, (void*)init);
+	}
+
+	for (i = 0; i < INSERTERS; i++) {
+		pthread_create(&insert_procs[i], NULL, inserter, (void*)init);
+	}
+
+	// thread termination and clean-up
+	cleanUpThreads(search_procs, SEARCHERS);
+	cleanUpThreads(insert_procs, INSERTERS);
+
+	return 0;
+}
